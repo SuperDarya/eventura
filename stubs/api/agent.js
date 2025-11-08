421e1999a251b91d789199f0cf8b0656befad4c0
grinikita <nekitboy@yandex.ru>
2025-11-08 13:12:22 +0300

    agent langgraph

--- stubs/api/agent.js
+++ stubs/api/agent.js
@@ -7,6 +7,9 @@ const { createReactAgent } = require('@langchain/langgraph/prebuilt')
 const { tool } = require('@langchain/core/tools')
 const z = require('zod')
 const { HumanMessage } = require('@langchain/core/messages')
+const { v4: uuid  } = require('uuid')
+const express = require('express')
+const { MemorySaver } = require('@langchain/langgraph')
 
 
 const weather = tool(
@@ -20,9 +23,9 @@ const weather = tool(
   },
   {
     name: "weather",
-    description: "Get information about current weather",
+    description: "Get information about current weather. Если человек не указал город, то уточни у него",
     schema: z.object({
-      city: z.string().describe("City in which we would get weather information"),
+      city: z.string().describe("Город, в котором нужна погода со слов пользователя. Обязательный параметр"),
       date: z.string().optional().describe("This is optional parameter. If it exists weather information would be by that date. Format: dd.mm.yyyy")
     }),
   }
@@ -38,26 +41,46 @@ const giga = new GigaChat({
   httpsAgent
 })
 
+
 const agent = createReactAgent({
   llm: giga,
   tools: [weather],
-  prompt: 'Answer on English'
+  prompt: `Ты отвечаешь на вопрос человека о погоде. Не придумывай город по умолчанию сам. Если человеу не указал город, то спроси в каком городе запросить погоду
+  
+  <Пример>
+  Пользователь - Какая сейчас погода?
+  Ответ - В каком городе погода тебя интересует?
+  </Пример>
+  `,
+  checkpointer: new MemorySaver()
 })
 
-const call = async (message) => {
+const call = async (message, sessionId) => {
   const response = await agent.invoke({
     messages: [new HumanMessage(message)]
-  })
+  }, { configurable: { thread_id: sessionId } })
 
-  // console.log(response)
-  const res2 = await agent.invoke({
-    messages: [...response.messages, new HumanMessage('What about Moscow?')]
-  })
-  console.log(res2)
+  return response
 }
 
 
-call('What weather in Innopolis?')
 
-// session-id - for gigachat
-// session-id - for working with message history (short-term-memory)
+// TODO x-session-id - for gigachat
+
+const agentRouter = express.Router()
+
+agentRouter.post('/prompt', async (req, res) => {
+  const { message, sessionId = uuid() } = req.body
+
+  const answer = await call(message, sessionId)
+
+  res.send({
+    message: answer.messages.at(-1).content,
+    sessionId: sessionId
+  })
+})
+
+module.exports = {
+  agentRouter,
+  agent
+}
--- stubs/api/index.js
+++ stubs/api/index.js
@@ -12,6 +12,7 @@ const { getModel, gigachat } = require('./llm')
 const authRouter = require('./auth')
 const booksRouter = require('./books')
 const analyticsRouter = require('./analytics')
+const agentRouter = require('./agent')
 
 
 const router = Router()
@@ -22,116 +23,117 @@ router.use(timer());
 router.use('/analytics', analyticsRouter)
 router.use('/auth', authRouter)
 router.use('/books', booksRouter)
+router.use('/agent', agentRouter)
 
 
-const slideDataSchema = z.object({
-    title: z.string().describe("Название товара"),
-    description: z.string().describe("Описание товара. Около 100 слов"),
-    price: z.number().describe("Цена товара"),
-    rating: z.number().describe("Рейтинг товара от 0 до 5"),
-    comments: z.array(z.object({
-        name: z.string().describe("Имя комментатора"),
-        text: z.string().describe("Текст комментария"),
-        rating: z.number().describe("Оценка комментария от 0 до 5"),
-    })).describe("Комментарии к товару. Положительные и отрицательные. Минимум 3 комментария"),
-    imagePrompt: z.string().describe('Промт для генерации изображения. Перечисли объекты которые необходимо нарисовать через запятую настройки камеры и окружение. Если изображение рисоваать не требуется напиши "no image"'),
-})
-
-const zodSchema = z.object({
-  catalogName: z.string().describe("Название каталога на латинице не более 12 символов"),
-  imagesStyle: z.string().describe("Укажи стиль изображения и цветовую гамму"),
-  cards: z.array(slideDataSchema).describe('Список карточек'),
-});
-
-const instructionsCreator = StructuredOutputParser.fromZodSchema(zodSchema);
-
-const chain = RunnableSequence.from([
-  ChatPromptTemplate.fromTemplate(
-    `Создай каталог товаров.
-    Пользователь описал каталог так: {question}
-    Язык каталога {lang}
-    Количествво карточек: {count} шт.
-    {format_instructions}
-    
-    Текущая дата ${new Date().toLocaleDateString()}`
-  ),
-  getModel('GigaChat-2-Pro'),
-  new JsonOutputParser(),
-]);
-
-const main = async ({
-    title,
-    count,
-    lang
-}) => {
-    const stream = await chain.stream({
-      question: title,
-      count,
-      lang,
-      format_instructions: instructionsCreator.getFormatInstructions(),
-    });
-    
-    return stream;
-}
-
-router.get('/generate', async (req, res) => {
-  const { title, count, lang } = req.query
-
-  const stream = await main({
-    title,
-    lang,
-    count: parseInt(count),
-  })
-
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream; charset=utf-8',
-    'Cache-Control': 'no-cache'
-  });
-
-  for await (const s of stream) {
-    res.write(`data: ${JSON.stringify(s)}\n\n`);
-  }
-
-  res.write(`event: end\ndata: { "done": true }\n\n`);
-  res.end();
-})
-
-router.post('/generate-image', async (req, res) => {
-  const { imagePrompt, imagesStyle } = req.body
-
-  const chatResponse = await gigachat.chat({
-    messages: [
-      {
-        role: 'user',
-        content: `Создай изображение по запросу ${imagePrompt} в стиле ${imagesStyle}`
-      }
-    ],
-    function_call: 'auto'
-  })
-
-  const detectedImage = detectImage(chatResponse.choices[0]?.message.content ?? '');
-  if (!detectedImage || !detectedImage.uuid) {
-    console.log('Image not found')
-    console.log(chatResponse.choices[0]?.message)
-    return {}
-  }
-
-  const image = await gigachat.getImage(detectedImage.uuid);
-
-  const fs = await import('node:fs')
-  fs.writeFileSync(path.resolve(__dirname, '..', `images/${detectedImage.uuid}.png`), Uint8Array.from(Array.from(image.content) .map(
-    (letter) => letter.charCodeAt(0)
-  )))
-
-  res.send({
-    uuid: detectedImage.uuid
-  })
-})
-
-router.get('/card-image/:uuid', async (req, res) => {
-  const { uuid } = req.params
-
-  res.sendFile(path.resolve(__dirname, '..', `images/${uuid}.png`))
-})
+// const slideDataSchema = z.object({
+//     title: z.string().describe("Название товара"),
+//     description: z.string().describe("Описание товара. Около 100 слов"),
+//     price: z.number().describe("Цена товара"),
+//     rating: z.number().describe("Рейтинг товара от 0 до 5"),
+//     comments: z.array(z.object({
+//         name: z.string().describe("Имя комментатора"),
+//         text: z.string().describe("Текст комментария"),
+//         rating: z.number().describe("Оценка комментария от 0 до 5"),
+//     })).describe("Комментарии к товару. Положительные и отрицательные. Минимум 3 комментария"),
+//     imagePrompt: z.string().describe('Промт для генерации изображения. Перечисли объекты которые необходимо нарисовать через запятую настройки камеры и окружение. Если изображение рисоваать не требуется напиши "no image"'),
+// })
+//
+// const zodSchema = z.object({
+//   catalogName: z.string().describe("Название каталога на латинице не более 12 символов"),
+//   imagesStyle: z.string().describe("Укажи стиль изображения и цветовую гамму"),
+//   cards: z.array(slideDataSchema).describe('Список карточек'),
+// });
+//
+// const instructionsCreator = StructuredOutputParser.fromZodSchema(zodSchema);
+//
+// const chain = RunnableSequence.from([
+//   ChatPromptTemplate.fromTemplate(
+//     `Создай каталог товаров.
+//     Пользователь описал каталог так: {question}
+//     Язык каталога {lang}
+//     Количествво карточек: {count} шт.
+//     {format_instructions}
+//
+//     Текущая дата ${new Date().toLocaleDateString()}`
+//   ),
+//   getModel('GigaChat-2-Pro'),
+//   new JsonOutputParser(),
+// ]);
+//
+// const main = async ({
+//     title,
+//     count,
+//     lang
+// }) => {
+//     const stream = await chain.stream({
+//       question: title,
+//       count,
+//       lang,
+//       format_instructions: instructionsCreator.getFormatInstructions(),
+//     });
+//
+//     return stream;
+// }
+//
+// router.get('/generate', async (req, res) => {
+//   const { title, count, lang } = req.query
+//
+//   const stream = await main({
+//     title,
+//     lang,
+//     count: parseInt(count),
+//   })
+//
+//   res.writeHead(200, {
+//     'Content-Type': 'text/event-stream; charset=utf-8',
+//     'Cache-Control': 'no-cache'
+//   });
+//
+//   for await (const s of stream) {
+//     res.write(`data: ${JSON.stringify(s)}\n\n`);
+//   }
+//
+//   res.write(`event: end\ndata: { "done": true }\n\n`);
+//   res.end();
+// })
+//
+// router.post('/generate-image', async (req, res) => {
+//   const { imagePrompt, imagesStyle } = req.body
+//
+//   const chatResponse = await gigachat.chat({
+//     messages: [
+//       {
+//         role: 'user',
+//         content: `Создай изображение по запросу ${imagePrompt} в стиле ${imagesStyle}`
+//       }
+//     ],
+//     function_call: 'auto'
+//   })
+//
+//   const detectedImage = detectImage(chatResponse.choices[0]?.message.content ?? '');
+//   if (!detectedImage || !detectedImage.uuid) {
+//     console.log('Image not found')
+//     console.log(chatResponse.choices[0]?.message)
+//     return {}
+//   }
+//
+//   const image = await gigachat.getImage(detectedImage.uuid);
+//
+//   const fs = await import('node:fs')
+//   fs.writeFileSync(path.resolve(__dirname, '..', `images/${detectedImage.uuid}.png`), Uint8Array.from(Array.from(image.content) .map(
+//     (letter) => letter.charCodeAt(0)
+//   )))
+//
+//   res.send({
+//     uuid: detectedImage.uuid
+//   })
+// })
+//
+// router.get('/card-image/:uuid', async (req, res) => {
+//   const { uuid } = req.params
+//
+//   res.sendFile(path.resolve(__dirname, '..', `images/${uuid}.png`))
+// })
 
 module.exports = router;
