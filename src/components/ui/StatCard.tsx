dd47339d13876911373602a589df94edb231878c
SuperDarya <yakovleva8051@gmail.com>
2025-12-26 21:30:02 +0300

    исправлен баг с темной темой на главной странице

--- src/components/ui/StatCard.tsx
+++ src/components/ui/StatCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stat, StatLabel, StatNumber, StatHelpText, Box } from '@chakra-ui/react'
+import { Stat, StatLabel, StatNumber, StatHelpText, Box, useColorModeValue } from '@chakra-ui/react'
 
 interface StatCardProps {
   number: string
@@ -9,15 +9,19 @@ interface StatCardProps {
 }
 
 export const StatCard = ({ number, label, helpText, color = 'brand.400' }: StatCardProps) => {
+  const bgColor = useColorModeValue('white', 'gray.800')
+  const labelColor = useColorModeValue('gray.700', 'gray.200')
+  const helpTextColor = useColorModeValue('gray.500', 'gray.400')
+
   return (
-    <Stat textAlign="center" p={4} borderRadius="lg" bg="white" boxShadow="sm">
+    <Stat textAlign="center" p={4} borderRadius="lg" bg={bgColor} boxShadow="sm">
       <StatNumber fontSize="4xl" color={color}>
         {number}
       </StatNumber>
-      <StatLabel fontSize="lg" mt={2}>
+      <StatLabel fontSize="lg" mt={2} color={labelColor}>
         {label}
       </StatLabel>
-      {helpText && <StatHelpText>{helpText}</StatHelpText>}
+      {helpText && <StatHelpText color={helpTextColor}>{helpText}</StatHelpText>}
     </Stat>
   )
 }
